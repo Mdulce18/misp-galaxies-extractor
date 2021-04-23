ec5c1a4a49d245db976aad640c2f68298a78b698
Mdulce18 <mdulce@gmail.com>
2021-04-23 02:09:37 -0300

    main and clases update with description

--- actor_repository.py
+++ actor_repository.py
@@ -11,8 +11,9 @@ from actor import Actor
 class Actor_Repository():
     def __init__(self, misp_json_link):
         self.misp_galaxy_json_link = misp_json_link
+        # self.actors_aliases_list = []
         self.actors_list = []
-        self.description_list = []
+        # self.description_list = []
 
     def parse_json(self):
         try:
@@ -21,16 +22,19 @@ class Actor_Repository():
         except requests.exceptions.MissingSchema:
             logging.error("Invalid URL Error")
             return None
+        except requests.exceptions.InvalidSchema:
+            logging.error("Invalid URL Error")
+            return None
         except requests.exceptions.HTTPError:
             logging.error("Server Error: 500")
             return None
 
         return resp
 
-    def create_actors_list(self, json_text):
+    def create_actors_list(self):
         b = True  # b = True|None
         actor_counter = 0
-        json = json_text
+        json = self.parse_json()
 
         while b is True:
             new_actor = Actor(actor_counter)
@@ -41,22 +45,39 @@ class Actor_Repository():
                 raw_aliases = new_actor.get_raw_community_identifiers()
                 new_actor.format_data(
                     new_actor.get_raw_community_identifiers())
-                final_actor_list = new_actor.get_community_identifiers()
-                self.actors_list.append(final_actor_list)
-                self.description_list.append(new_actor.get_description())
+                new_actor.get_community_identifiers()
+                # self.actors_aliases_list.append(final_actor_list)
+                self.actors_list.append(new_actor)
                 actor_counter += 1
 
-        return True
+        return
+
+    def create_aliases_list(self):
+        aliases_list = []
+        for actor in self.actors_list:
+            aliases = actor.get_community_identifiers()
+            aliases_list.append(aliases)
+        return aliases_list
 
     def get_aliases_position(self, alias):
         actor_position = -1
-        for actor in self.actors_list:
+        aliases_list = self.create_aliases_list()
+        for actor in aliases_list:
             actor_position += 1
             if alias in actor:
                 return actor_position
+        return None
+
+    def get_aliases(self, alias):
+        position = self.get_aliases_position(alias)
+        return self.actors_list[position].community_identifiers_list
+
+    def get_description(self, alias):
+        position = self.get_aliases_position(alias)
+        return self.actors_list[position].description
+
+    def get_actors_aliases_list(self):
+        return self.actors_aliases_list
 
     def get_actors_list(self):
         return self.actors_list
-
-    def get_description_list(self):
-        return self.description_list
--- main.py
+++ main.py
@@ -19,12 +19,12 @@ def search_for_alias(repository, alias_name):
             aliases_position = repository.get_aliases_position(alias_format)
             # gather the position to search in repository of actor and descriptions
             if aliases_position is not None:
-                aliases_list = repository.get_actors_list()
-                description_list = repository.get_description_list()
-                aliases_formated = convert_list_to_string(
-                    aliases_list[aliases_position], ',')
+                aliases_list = repository.get_aliases(alias_format)
+                aliases_formated = convert_list_to_string(aliases_list, ',')
+                actor_description = repository.get_description(alias_format)
+
                 print(
-                    f'Aliases:\n{aliases_formated}\nDescription:\n{description_list[aliases_position]}')
+                    f'Aliases:\n{aliases_formated}\nDescription:\n{actor_description}')
 
             else:
                 logging.error('Alias not found')
@@ -35,10 +35,7 @@ def search_for_alias(repository, alias_name):
 def main():
     print(intro)
     new_repository = Actor_Repository(link)
-    json_text = new_repository.parse_json()
-    new_repository.create_actors_list(json_text)
-    new_repository.get_actors_list()
-    # print(full_repo)
+    new_repository.create_actors_list()
     alias_name = None
     search_for_alias(new_repository, alias_name)
     print('Exiting...')
